eafa14bc098133e071a3ab50aec0e8c848f3785c
Jose Ampuero <ampuerovillanueva@gmail.com>
2024-09-22 10:03:32 -0500

    Revert "pair2"
    This reverts commit 92a28941ca12902660039b1bcb07228c381b2171.

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export function Home() {
+export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
